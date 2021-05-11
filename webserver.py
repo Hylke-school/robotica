029fc94fd1d8746859165cf064f31badd8173a26
Hylkinator <hylke.van.der.zee@student.nhlstenden.com>
2021-05-11 14:32:22 +0200

    added interrupt handler, closes socket on interrupt

--- webserver.py
+++ webserver.py
@@ -1,20 +1,29 @@
 # Python 3 server example
 import socket
 
-ip = "141.252.29.5"
-port = 5535
-buffer_size = 4096
+global sock
 
-sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-sock.bind((ip, port))
-sock.listen(1)
 
-# print('Connection address:', ip)
-while True:
-    conn, addr = sock.accept()
+def receive_socket():
+    ip = "141.252.29.5"
+    port = 5535
+    buffer_size = 4096
+
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    sock.bind((ip, port))
+    sock.listen(1)
+
+    # print('Connection address:', ip)
     while True:
-        data = conn.recv(buffer_size)
-        if not data: break
-        print(data)
+        conn, addr = sock.accept()
+        while True:
+            data = conn.recv(buffer_size)
+            if not data: break
+            print(data)
 
-conn.close()
+
+try:
+    receive_socket()
+finally:
+    print("Error detected, closing socket")
+    sock.close()
