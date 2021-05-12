cb38265afdaf695d90f8aab6b85f1660cb1b0e8c
Hylkinator <hylke.van.der.zee@student.nhlstenden.com>
2021-05-12 09:44:54 +0200

    refactored webserver for two files, made socket receiver into class

---
+++ get_json.py
@@ -0,0 +1,13 @@
+import receive_remote.py
+
+ip = "141.252.29.5"
+port = 5535
+
+socket = MySocket(ip, port)
+try:
+    socket.connect_socket()
+    while True:
+        data = socket.get_data()
+        print(data)
+finally:
+    socket.close_socket()
---
+++ receive_remote.py
@@ -0,0 +1,25 @@
+import socket
+
+
+class MySocket:
+    def __init__(self, ip, port):
+        self.ip = ip
+        self.port = port
+        self.buffer_size = 4096
+        self.sock = None
+        self.conn = None
+        self.addr = None
+
+    def connect_socket(self):
+        self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.sock.bind((self.ip, self.port))
+        self.sock.listen(1)
+        self.conn, self.addr = self.sock.accept()
+
+    def get_data(self):
+        data = self.conn.recv(self.buffer_size)
+        return data
+
+    def close_socket(self):
+        print("Error detected, closing socket")
+        self.sock.close()
