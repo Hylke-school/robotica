316fcd53b0f27cf9554d7208178011a0f6632d9f
Hylkinator <hylke.van.der.zee@student.nhlstenden.com>
2021-05-17 10:43:53 +0200

    set to IP of raspi at Hylke's home set to increase values constantly

--- get_json.py
+++ get_json.py
@@ -5,13 +5,18 @@ from receive_remote import Socket
 
 class JSON:
     def __init__(self):
-        self.ip = "141.252.29.30"
+        self.ip = "192.168.0.205"
         self.port = 5535
         self.data = None
-        self.socket = Socket(self.ip, self.port)
-        self.socket.connect_socket()
+        self.counter = 0
+        # self.socket = Socket(self.ip, self.port)
+        # self.socket.connect_socket()
 
     def get_json(self):
-        self.data = self.socket.get_data()
-        # self.data = "{\"pot\": " + str(random.randrange(0, 1023)) + "}"
+        # self.data = self.socket.get_data()
+        self.data = "{\"pot\": " + str(self.counter) + "}"
+        if self.counter > 800:
+            self.counter = 0
+        else:
+            self.counter += 10
         return self.data
