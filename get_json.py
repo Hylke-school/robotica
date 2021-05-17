3a049f7092d3a24fa9d9a115f4017549f77aa3b4
Hylkinator <hylke.van.der.zee@student.nhlstenden.com>
2021-05-17 12:28:21 +0200

    made server send post requests to server at Hylke's home to allow telemetry access from non-local sources

--- get_json.py
+++ get_json.py
@@ -8,15 +8,15 @@ class JSON:
         self.ip = "141.252.29.30"
         self.port = 5535
         self.data = None
-        # self.counter = 0
+        #self.counter = 0
         self.socket = Socket(self.ip, self.port)
         self.socket.connect_socket()
 
     def get_json(self):
         self.data = self.socket.get_data()
-        # self.data = "{\"pot\": " + str(self.counter) + "}"
-        # if self.counter > 800:
-        #     self.counter = 0
-        # else:
-        #     self.counter += 10
+        #self.data = "{\"pot\": " + str(self.counter) + "}"
+        #if self.counter > 800:
+        #    self.counter = 0
+        #else:
+        #    self.counter += 10
         return self.data
--- server.py
+++ server.py
@@ -6,8 +6,8 @@ from get_json import JSON
 
 json = JSON()
 
-payload = json.get_json()
-
 while True:
-    r = requests.post("dns.hylke.xyz:5356", data=payload)
+    payload = json.get_json()
+    print(payload)
+    r = requests.post("http://dns.hylke.xyz:5356", data=payload)
     time.sleep(0.1)
