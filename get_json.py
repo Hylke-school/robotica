688e52383a7cede0ce0d605c97ccbb4c2517fa6a
Hylkinator <hylke.van.der.zee@student.nhlstenden.com>
2021-05-17 11:02:28 +0200

    set to accept data from esp again

--- get_json.py
+++ get_json.py
@@ -8,15 +8,15 @@ class JSON:
         self.ip = "192.168.0.205"
         self.port = 5535
         self.data = None
-        self.counter = 0
-        # self.socket = Socket(self.ip, self.port)
-        # self.socket.connect_socket()
+        # self.counter = 0
+        self.socket = Socket(self.ip, self.port)
+        self.socket.connect_socket()
 
     def get_json(self):
-        # self.data = self.socket.get_data()
-        self.data = "{\"pot\": " + str(self.counter) + "}"
-        if self.counter > 800:
-            self.counter = 0
-        else:
-            self.counter += 10
+        self.data = self.socket.get_data()
+        # self.data = "{\"pot\": " + str(self.counter) + "}"
+        # if self.counter > 800:
+        #     self.counter = 0
+        # else:
+        #     self.counter += 10
         return self.data
