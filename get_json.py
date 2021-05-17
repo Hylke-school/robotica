7e48a66c43a0a057d69cb643bc6e1770d63236d1
Hylkinator <hylke.van.der.zee@student.nhlstenden.com>
2021-05-17 10:27:06 +0200

    refactored to not depend on esp

--- get_json.py
+++ get_json.py
@@ -8,10 +8,15 @@ class JSON:
         self.ip = "141.252.29.30"
         self.port = 5535
         self.data = None
-        self.socket = Socket(self.ip, self.port)
-        self.socket.connect_socket()
+        self.counter = 0
+        # self.socket = Socket(self.ip, self.port)
+        # self.socket.connect_socket()
 
     def get_json(self):
-        self.data = self.socket.get_data()
-        # self.data = "{\"pot\": " + str(random.randrange(0, 1023)) + "}"
+        # self.data = self.socket.get_data()
+        self.data = "{\"pot\": " + str(self.counter) + "}"
+        if (self.counter > 800):
+            self.counter = 0
+        else:
+            self.counter += 10
         return self.data
