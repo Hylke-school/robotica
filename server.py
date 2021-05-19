9366e76945ce0a252c5de6fb2d70651e4d900c06
Hylkinator <hylke.van.der.zee@student.nhlstenden.com>
2021-05-19 15:13:21 +0200

    prevented printing empty strings

--- server.py
+++ server.py
@@ -6,6 +6,7 @@ json = JSON()
 
 while True:
     payload = json.get_json()
-    print(payload)
+    if payload != "":
+        print(payload)
     # r = requests.post("http://dns.hylke.xyz:5356", data=payload)
     time.sleep(0.1)
