68469a851e95f726a64d34eb37113c4f0a7f269d
Rick <rickkruithof21@gmail.com>
2021-05-19 11:03:47 +0200

    added vision for blue block

--- main.py
+++ main.py
@@ -21,8 +21,44 @@ def visiontest():
     cap.release()
     cv2.destroyAllWindows()
 
+def blauwBlokje():
+    cap = cv2.VideoCapture(0)
+    cap_width_middle = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH)/2)
+    cap_height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+    while(True):
+        _, frame = cap.read()
+
+        hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+
+        lower_blue = np.array([100,80,50])
+        upper_blue = np.array([110,255,255])
+        mask_blue = cv2.inRange(hsv, lower_blue, upper_blue)
+
+        blue_res = cv2.bitwise_and(frame, frame, mask= mask_blue)
+
+        gray = cv2.cvtColor(blue_res, cv2.COLOR_BGR2GRAY)
+        _,trash = cv2.threshold(gray, 0,255,0)
+
+        contours, _ = cv2.findContours(trash, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+
+        for cnt in contours:
+            if cv2.contourArea(cnt) <= 500:
+                continue
+            x,y,w,h = cv2.boundingRect(cnt)
+            if x > cap_width_middle:
+                print("right")
+            elif x+w < cap_width_middle:
+                print("left")
+            else:
+                print("middle")
+            cv2.rectangle(frame, (x,y),(x+w,y+h), (0,255,0),2)
+        cv2.line(frame, (cap_width_middle, 0), (cap_width_middle, cap_height), (0,255,0),2)
+        cv2.imshow('window', frame)
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+
 def main():
-    visiontest()
+    blauwBlokje()
 
 if __name__ == "__main__":
     main()
