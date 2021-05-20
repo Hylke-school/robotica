b747ea1ba4c7aa7edc1070227753c49f63fa36d9
Hylkinator <hylke.van.der.zee@student.nhlstenden.com>
2021-05-20 11:04:34 +0200

    tried something with locks

--- receive_remote.py
+++ receive_remote.py
@@ -1,12 +1,7 @@
 import socket
 import threading
+from threading import Lock
 
-
-def get_data():
-    f = open("data.json", "r")
-    value = f.read()
-    f.close()
-    return value
 
 
 class Socket:
@@ -17,6 +12,8 @@ class Socket:
         self.sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         self.sock.bind((self.ip, self.port))
         self.thread = None
+        self.lock = Lock()
+        self.data = None
 
     def start_loop(self):
         self.thread = threading.Thread(target=self.loop, daemon=True)
@@ -25,9 +22,21 @@ class Socket:
     def loop(self):
         while True:
             data, addr = self.sock.recvfrom(1024)
-            f = open("data.json", "w")
-            f.write(data.decode('utf-8'))
-            f.close()
+            self.lock.acquire()
+            self.data = data
+            self.lock.release()
+            # f = open("data.json", "w")
+            # f.write(data.decode('utf-8'))
+            # f.close()
 
     def stop_loop(self):
         self.join()
+
+    def get_data(self):
+        self.lock.acquire()
+        value = self.data
+        self.lock.release()
+        # f = open("data.json", "r")
+        # value = f.read()
+        # f.close()
+        return value
