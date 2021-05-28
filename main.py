5a75b15caaee47d977f9616553db2f2ffadb08ad
Hylkinator <hylke.van.der.zee@student.nhlstenden.com>
2021-05-28 10:01:48 +0200

    added live stream to telemetry server

--- main.py
+++ main.py
@@ -2,7 +2,6 @@ import math
 
 import cv2
 import numpy as np
-import flask
 from flask import Response, render_template, Flask
 
 app = Flask(__name__)
@@ -33,7 +32,7 @@ def blauwBlokje():
     cap = cv2.VideoCapture(0)
     cap_width_middle = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH) / 2)
     cap_height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
-    while True:
+    while (True):
         _, frame = cap.read()
 
         hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
@@ -68,64 +67,56 @@ def blauwBlokje():
 
 def findCap():
     cap = cv2.VideoCapture(0)
+    cap_width_middle = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH)/2)
+    cap_height_middle = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT)/2)
 
-    while True:
+    while(True):
         _, frame = cap.read()
-        frame = cv2.flip(frame, 1)
+        frame = cv2.flip(frame,1)
         gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-        # edges ofzo
+        #edges ofzo
         bilateral = cv2.bilateralFilter(frame, 5, 175, 175)
         edges = cv2.Canny(bilateral, 75, 200)
-        contours, _ = cv2.findContours(edges, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+        contours,_ = cv2.findContours(edges, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
 
         cntlist = []
         for cnt in contours:
             perimeter = cv2.arcLength(cnt, True)
-            approx = cv2.approxPolyDP(cnt, 0.01 * perimeter, True)
+            approx = cv2.approxPolyDP(cnt, 0.01*perimeter, True)
             area = cv2.contourArea(cnt)
-            if perimeter == 0:
+            if perimeter ==0:
                 continue
-            factor = 4 * math.pi * area / perimeter ** 2
+            factor = 4*math.pi*area/perimeter**2
             if ((len(approx) > 8) & (area > 400) & (factor > 0.7)):
                 cntlist.append(cnt)
-        cv2.drawContours(frame, cntlist, -1, (255, 0, 0), 2)
+        if cntlist != []:
+            target = [0,None]
+            for cnt in cntlist:
+                x,y,w,h = cv2.boundingRect(cnt)
+                if(y+h)>target[0]:
+                    target[0] = y+h
+                    target[1] = cnt
+            if target[1] is not None:
+                cv2.drawContours(frame, target[1], -1, (255,0,0),2)
+            x,y,w,h = cv2.boundingRect(target[1])
+            mid_x = int(x+w/2)
+            mid_y = int(y+h/2)
+            values = [0,0]
+            values[0] = round(-1 + mid_x/cap_width_middle, 2)
+            values[1] = round(-1 + mid_y/cap_height_middle,2)
+            cv2.circle(frame, (mid_x,mid_y),5,(0,255,0),1)
+            print("dx: "+str(values[0])+" dy: "+str(values[1]))
 
-        # hough circles
-        # circles = cv2.HoughCircles(gray, cv2.HOUGH_GRADIENT, 1.2, 100)
-
-        # if circles is not None:
-        #     circles = np.round(circles[0, :]).astype("int")
-        #     for (x,y,r) in circles:
-        #         cv2.circle(frame, (x,y), r, (0,255,0),2)
-
-        # manual
-        # blur = cv2.GaussianBlur(gray, (9,9),0)
-
-        # _,th = cv2.threshold(blur, 100, 255, cv2.THRESH_BINARY_INV)
-        # contours, hierarchy = cv2.findContours(th, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-        # for cnt in contours:
-        #     area = cv2.contourArea(cnt)
-        #     if area < 100:
-        #         continue
-        #     perimeter = cv2.arcLength(cnt, True)
-        #     if perimeter == 0:
-        #         continue
-        #     factor = 4 * math.pi * area / perimeter**2
-        #     if factor > 0.7:
-        #         cv2.drawContours(frame, cnt, -1, (255,0,0),2)
-        #     else:
-        #         cv2.drawContours(frame, cnt, -1, (0,255,0),2)
-
-        # cv2.imshow("gray",gray)
-        # cv2.imshow("threshold",th)
-
-        ret, buffer = cv2.imencode('.jpg', frame)
-        frame = buffer.tobytes()
-        yield b'--frame\r\n'b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n'
+        cv2.line(frame, (cap_width_middle-50,cap_height_middle),(cap_width_middle+50,cap_height_middle),(0,255,0),1)
+        cv2.line(frame, (cap_width_middle,cap_height_middle-50),(cap_width_middle,cap_height_middle+50),(0,255,0),1)
 
         # cv2.imshow('window', frame)
         # if cv2.waitKey(1) & 0xFF == ord('q'):
         #     break
+
+        ret, buffer = cv2.imencode('.jpg', frame)
+        frame = buffer.tobytes()
+        yield b'--frame\r\n'b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n'
 
 
 def main():
@@ -136,9 +127,11 @@ def main():
 def index():
     return render_template('index.html')
 
+
 @app.route('/video_feed')
 def video_feed():
     return Response(findCap(), mimetype='multipart/x-mixed-replace; boundary=frame')
+
 
 if __name__ == "__main__":
     app.run(debug=True, host="141.252.29.66")
