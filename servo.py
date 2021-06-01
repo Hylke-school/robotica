a015fd70f1774ef922d785793e37d2df4d0c08f1
Ubuntu <ubuntu@ubuntu.nhl.nl>
2021-06-01 13:14:09 +0000

    deleted README, edited loaaads of stuff

--- servo.py
+++ servo.py
@@ -1,39 +1,85 @@
 from time import sleep
-import serial
+import math
 import RPi.GPIO as GPIO
+import serial
+
+
+class Neck:
+    def __init__(self):
+        self.bottom = Servo(0)
+        self.top = Servo(1)
+
+    def change_position(self, level):
+        # level van 0 tot 100
+        # 0 = liggend, 100 = staand
+        position = 0.9 * level - 90
+        self.top.set_position(position)
+        self.bottom.set_position(position)
 
 
 class Servo:
+    def __init__(self, servo_id):
+        self.serial = ServoSerial()
+        self.id = servo_id
+
+    def set_position(self, degrees):
+        self.serial.set_position(self.id, degrees)
+
+
+class ServoSerial:
+    class __impl:
+        def __init__(self):
+            self.serial = serial.Serial(port='/dev/ttyS0', baudrate=57600, bytesize=serial.EIGHTBITS,
+                                        parity=serial.PARITY_NONE, stopbits=serial.STOPBITS_ONE, write_timeout=10)
+            GPIO.setmode(GPIO.BOARD)
+            GPIO.setup(18, GPIO.OUT)
+
+        def send(self, array):
+            array = self.get_array(array)
+            GPIO.output(18, GPIO.HIGH)
+            self.serial.reset_input_buffer()
+            self.serial.reset_output_buffer()
+            self.serial.write(bytearray(array))
+            sleep(0.05)
+            GPIO.output(18, GPIO.LOW)
+            print(self.serial.read_all())
+
+        def get_array(self, array):
+            header = [0xFF, 0xFF]
+            total = 0
+            for i in array:
+                total += i
+            checksum = (~total) & 0xFF
+            header.extend(array)
+            header.append(checksum)
+            return header
+
+        def set_position(self, servo_id, degrees):
+            position = math.floor((degrees + 150.) / 300. * 1023.)
+            upper_pos = position // 256
+            lower_pos = position % 256
+            self.send([servo_id, 0x05, 0x03, 30, lower_pos, upper_pos])
+
+        def construct_send(self, servo_id, instruction, address, par1, par2):
+            self.send([servo_id, 0x05, instruction, address, par1, par2])
+
+    # storage for the instance reference
+    __instance = None
+
     def __init__(self):
-        self.serial = serial.Serial(port='/dev/ttyS0', baudrate=57600, bytesize=serial.EIGHTBITS,
-                                    parity=serial.PARITY_NONE, stopbits=serial.STOPBITS_ONE, write_timeout=10)
+        """ Create singleton instance """
+        # Check whether we already have an instance
+        if ServoSerial.__instance is None:
+            # Create and remember instance
+            ServoSerial.__instance = ServoSerial.__impl()
 
-        GPIO.setmode(GPIO.BOARD)
-        GPIO.setup(18, GPIO.OUT)
+        # Store instance reference as the only member in the handle
+        self.__dict__['_Singleton__instance'] = ServoSerial.__instance
 
-    def send(self, array):
-        array = self.get_array(array)
-        GPIO.output(18, GPIO.HIGH)
-        self.serial.reset_input_buffer()
-        self.serial.reset_output_buffer()
-        self.serial.write(bytearray(array))
-        sleep(0.05)
-        GPIO.output(18, GPIO.LOW)
-        print(self.serial.read_all())
+    def __getattr__(self, attr):
+        """ Delegate access to implementation """
+        return getattr(self.__instance, attr)
 
-    @staticmethod
-    def get_array(array):
-        header = [0xFF, 0xFF]
-        total = 0
-        for i in array:
-            total += i
-        checksum = (~total) & 0xFF
-        header.extend(array)
-        header.append(checksum)
-        return header
-
-    def construct_send(self, servo_id, instruction, address, par1, par2):
-        self.send([servo_id, 0x05, instruction, address, par1, par2])
-
-    # def construct_send(self, servo_id, instruction, address, par1):
-    #     self.send([servo_id, 0x05, instruction, address, par1])
+    def __setattr__(self, attr, value):
+        """ Delegate access to implementation """
+        return setattr(self.__instance, attr, value)
--- test2.py
+++ test2.py
@@ -1,9 +1,21 @@
-from servo import Servo
+from time import sleep
+
+from servo import Neck, ServoSerial, Servo
+import serial as piserial
 
 
 def main():
-    servo = Servo()
-    servo.construct_send(0x00, 0x03, 30, 0x00, 0x01)
+    neck = Neck()
+    neck.change_position(0)
+    sleep(2)
+    neck.change_position(50)
+    # servo = Servo(0)
+    # servo2 = Servo(1)
+    # servo = ServoSerial()
+    # servo.construct_send(0x01, 0x03, 30, 0x00, 0x03)
+
+    # servo.set_position(0)
+    # servo2.set_position(90)
 
 
 if __name__ == "__main__":
